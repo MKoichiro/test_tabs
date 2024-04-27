9fdce55455b8f7c984e2c866ea8db8a42b97984d
Koichiro Mikama <koichiro.mika@gmail.com>
2024-04-27 14:44:16 +0900

    swcを導入(@vitejs/plugin-react-swc)

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react'
 import path from 'path'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { fileURLToPath } from 'url'
-import { dirname } from 'path'
+import { dirname } from 'path';
+import react from '@vitejs/plugin-react-swc';
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default defineConfig({
   plugins: [
-    reactRefresh(),
-    tsconfigPaths()
+    tsconfigPaths(),
+    react()
   ],
   resolve: {
     alias: {
